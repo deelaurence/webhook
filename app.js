9ca76ee1caa958133273ff6b845474f92e5a9a6a
deelaurence <laurie143pelumi@gmail.com>
2023-05-17 13:26:59 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+
+app.post('/webhook', (req, res) => {
+    // Process the webhook payload here
+    console.log('Received webhook:', req.body);
+    res.sendStatus(200);
+});
+
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
