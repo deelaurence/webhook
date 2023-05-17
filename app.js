6d5591dd6c9d056ff189a6a78e1986cbe8643200
deelaurence <laurie143pelumi@gmail.com>
2023-05-17 14:18:14 +0100

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
-
+app.get("/", (req, res) => {
+    res.send("hello")
+})
 app.post('/webhook', (req, res) => {
     // Process the webhook payload here
     console.log('Received webhook:', req.body);
